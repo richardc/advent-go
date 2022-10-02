ea450c97b4069528459a8b92d5ffafadd95ae867
Richard Clamp <richardc@unixbeard.net>
2022-10-02 21:14:01 +0100

    Day 5 Part 1 Checking Naughty vs Nice
    Use of switch to avoid long if chains isn't too terrible.

--- day05/main.go
+++ day05/main.go
@@ -3,12 +3,42 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 
 	"advent2015/pkg/input"
 	"advent2015/pkg/slices"
 )
 
 func isNice(s string) bool {
+	switch {
+	case
+		strings.Contains(s, "ab"),
+		strings.Contains(s, "cd"),
+		strings.Contains(s, "pq"),
+		strings.Contains(s, "xy"):
+		return false
+	}
+
+	vowels := strings.Count(s, "a") +
+		strings.Count(s, "e") +
+		strings.Count(s, "i") +
+		strings.Count(s, "o") +
+		strings.Count(s, "u")
+
+	if vowels < 3 {
+		return false
+	}
+
+	return doubledChar(s)
+}
+
+func doubledChar(s string) bool {
+	bytes := []byte(s)
+	for i, c := range bytes[:len(bytes)-1] {
+		if c == s[i+1] {
+			return true
+		}
+	}
 	return false
 }
 
