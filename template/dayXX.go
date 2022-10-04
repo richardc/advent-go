64480e2a0ea96d93a0425eb54636dbe933b96033
Richard Clamp <richardc@unixbeard.net>
2022-10-04 15:29:34 +0100

    Update the template based on recent usage
    This is closer to the pattern we've been seeing for the past few days,
    so capture that.

--- template/dayXX.go
+++ template/dayXX.go
@@ -3,19 +3,26 @@ package dayXX
 import (
 	_ "embed"
 
+	"github.com/richardc/advent-go/input"
 	"github.com/richardc/advent-go/runner"
+	"github.com/richardc/advent-go/slices"
 )
 
 //go:embed input.txt
-var input string
+var puzzle string
 
 func init() {
 	runner.Register(runner.Solution{
-		Day:   0,
-		Part1: func(any) any { return XXX(input) },
+		Day:   99,
+		Input: func() any { return input.Lines(puzzle) },
+		Part1: func(i any) any { return sumXXX(i.([]string)) },
 	})
 }
 
 func XXX(s string) int {
 	return 0
+}
+
+func sumXXX(s []string) int {
+	return slices.Sum(slices.Map(s, XXX))
 }
