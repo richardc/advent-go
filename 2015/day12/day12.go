e6153ab444bfbd5880e25e199f22cda0579fff53
Richard Clamp <richardc@unixbeard.net>
2022-10-06 11:51:09 +0100

    Day 12 Part 2 - Skip over dictionaries with "red" value

--- 2015/day12/day12.go
+++ 2015/day12/day12.go
@@ -15,6 +15,7 @@ func init() {
 	runner.Register(runner.Solution{
 		Day:   12,
 		Part1: func(any) any { return sumNumbers(puzzle) },
+		Part2: func(any) any { return sumNonRed(puzzle) },
 	})
 }
 
@@ -45,4 +46,40 @@ func sumNumbers(s string) float64 {
 	}
 
 	return eval(document)
+}
+
+func evalSkipping(d any) float64 {
+	switch d := d.(type) {
+	case float64:
+		return d
+	case []any:
+		total := 0.0
+		for _, v := range d {
+			total += evalSkipping(v)
+		}
+		return total
+	case map[string]any:
+		for _, v := range d {
+			if s, ok := v.(string); ok {
+				if s == "red" {
+					return 0
+				}
+			}
+		}
+		total := 0.0
+		for _, v := range d {
+			total += evalSkipping(v)
+		}
+		return total
+	}
+	return 0
+}
+
+func sumNonRed(s string) float64 {
+	var document any
+	if err := json.Unmarshal([]byte(s), &document); err != nil {
+		fmt.Println(err)
+	}
+
+	return evalSkipping(document)
 }
--- 2015/day12/day12_test.go
+++ 2015/day12/day12_test.go
@@ -22,3 +22,23 @@ func TestSumNumbers(t *testing.T) {
 		})
 	}
 }
+
+func TestSumNonRed(t *testing.T) {
+	tests := []struct {
+		input string
+		value float64
+	}{
+		{`[1,2,3]`, 6},
+		{`[1,{"c":"red","b":2},3]`, 4},
+	}
+
+	for _, testcase := range tests {
+		testname := testcase.input
+		t.Run(testname, func(t *testing.T) {
+			ans := sumNonRed(testcase.input)
+			if ans != testcase.value {
+				t.Errorf("got %v, want %v", ans, testcase.value)
+			}
+		})
+	}
+}
