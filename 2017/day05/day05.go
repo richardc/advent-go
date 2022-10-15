8ec5c2fc4ce21462856a6b8d5263654d51f33a88
Richard Clamp <richardc@unixbeard.net>
2022-10-15 17:00:07 +0100

    2017:5:2 Jumping but with some branchine

--- 2017/day05/day05.go
+++ 2017/day05/day05.go
@@ -17,6 +17,7 @@ func init() {
 			Year:  2017,
 			Day:   5,
 			Part1: func(any) any { return jumps(slices.Map(input.Lines(puzzle), input.MustAtoi)) },
+			Part2: func(any) any { return jumpsOddly(slices.Map(input.Lines(puzzle), input.MustAtoi)) },
 		},
 	)
 }
@@ -28,6 +29,22 @@ func jumps(program []int) int {
 		steps++
 		next := pc + program[pc]
 		program[pc]++
+		pc = next
+	}
+	return steps
+}
+
+func jumpsOddly(program []int) int {
+	pc := 0
+	steps := 0
+	for pc >= 0 && pc < len(program) {
+		steps++
+		next := pc + program[pc]
+		if program[pc] >= 3 {
+			program[pc]--
+		} else {
+			program[pc]++
+		}
 		pc = next
 	}
 	return steps
