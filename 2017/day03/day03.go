b6d0bdf25c7a7bdb891e41418403ad79a4b35d72
Richard Clamp <richardc@unixbeard.net>
2022-10-15 15:10:11 +0100

    2017:3:2 Calculate the neighbour sums
    We split the existing spiralManattan function to take a predicate to
    indicate if it should keep search.  The predicate takes the x,y
    coordinates and returns a bool.  For the original case we can ignore the
    arguments and just track that we've been called n times.
    
    For part 2 we can supply a more interesting predicate that computes the
    neighbour sums in a closed-over hash, and will stop looking once the sum
    exceeds the puzzle input.

--- 2017/day03/day03.go
+++ 2017/day03/day03.go
@@ -7,6 +7,8 @@ import (
 	"github.com/richardc/advent-go/input"
 	"github.com/richardc/advent-go/math"
 	"github.com/richardc/advent-go/runner"
+	"github.com/richardc/advent-go/slices"
+	"golang.org/x/exp/maps"
 )
 
 //go:embed "input.txt"
@@ -18,16 +20,52 @@ func init() {
 			Year:  2017,
 			Day:   3,
 			Part1: func(any) any { return spiralManhattan(input.MustAtoi(strings.TrimSpace(puzzle))) },
+			Part2: func(any) any { return spiralManhattanSums(input.MustAtoi(strings.TrimSpace(puzzle))) },
 		},
 	)
 }
 
 func spiralManhattan(puzzle int) int {
+	index := 1
+	return spiralManhattanTours(puzzle, func(x, y int) bool {
+		//fmt.Printf("%d %d %d,%d", index, puzzle, x, y)
+		index++
+		return index <= puzzle
+	})
+}
+
+func spiralManhattanSums(puzzle int) int {
+	sums := map[[2]int]int{}
+	neighbours := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	spiralManhattanTours(puzzle, func(x, y int) bool {
+		value := 0
+		if x == 0 && y == 0 {
+			// basecase, we have no neighbours, so just have the value 1
+			value = 1
+		} else {
+			for _, neighbour := range neighbours {
+				value += sums[[2]int{x + neighbour[0], y + neighbour[1]}]
+			}
+		}
+		sums[[2]int{x, y}] = value
+
+		return value <= puzzle
+	})
+
+	return slices.Max(maps.Values(sums))
+}
+
+func spiralManhattanTours(puzzle int, visit func(x, y int) bool) int {
 	x, y := 0, 0
 	minx, maxx := 0, 0
 	miny, maxy := 0, 0
 	direction := 0
-	for n := 1; n < puzzle; n++ {
+	for visit(x, y) {
 		//fmt.Printf("%d: %d,%d %d\n", n, x, y, math.Abs(x)+math.Abs(y))
 		switch direction {
 		case 0:
