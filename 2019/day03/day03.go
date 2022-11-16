402b8ba03497b2ab114c669eca21348ec4db1b01
Richard Clamp <richardc@unixbeard.net>
2022-11-16 18:16:58 +0000

    2019:3:2 Track the steps to each square
    Only slight funtime is not updating an existing entry.  Initially I'd
    read that as being more complex, resetting the steps back down when
    crossing over itself, but that wasn't intended, simply that the second
    trip through a point retain the steps for the first visit.

--- 2019/day03/day03.go
+++ 2019/day03/day03.go
@@ -19,7 +19,9 @@ func init() {
 		runner.Solution{
 			Year:  2019,
 			Day:   3,
-			Part1: func(any) any { return solve(puzzle) },
+			Input: func() any { closest, smallest := solve(puzzle); return []int{closest, smallest} },
+			Part1: func(i any) any { return i.([]int)[0] },
+			Part2: func(i any) any { return i.([]int)[1] },
 		},
 	)
 }
@@ -38,14 +40,17 @@ func newLink(s string) Link {
 
 type Point [2]int
 
-func solve(puzzle string) int {
+func solve(puzzle string) (int, int) {
 	wires := slices.Map(input.Lines(puzzle), func(s string) []Link { return slices.Map(strings.Split(s, ","), newLink) })
 	points := map[Point]int{}
+	steps := [2]map[Point]int{}
 	for i, wire := range wires {
 		bit := i + 1
 		x, y := 0, 0
+		step := 0
+		steps[i] = map[Point]int{}
 		for _, link := range wire {
-			for i := 0; i < link.distance; i++ {
+			for j := 0; j < link.distance; j++ {
 				switch link.direction {
 				case 'D':
 					y--
@@ -56,11 +61,17 @@ func solve(puzzle string) int {
 				case 'R':
 					x++
 				}
+				step++
+				if _, ok := steps[i][Point{x, y}]; !ok {
+					steps[i][Point{x, y}] = step
+				}
 				points[Point{x, y}] |= bit
 			}
 		}
 	}
 
 	crossed := slices.Filter(maps.Keys(points), func(p Point) bool { return points[p] == 3 })
-	return slices.Min(slices.Map(crossed, func(p Point) int { return math.Abs(p[0]) + math.Abs(p[1]) }))
+	closest := slices.Min(slices.Map(crossed, func(p Point) int { return math.Abs(p[0]) + math.Abs(p[1]) }))
+	smallest := slices.Min(slices.Map(crossed, func(p Point) int { return steps[0][p] + steps[1][p] }))
+	return closest, smallest
 }
--- 2019/day03/day03_test.go
+++ 2019/day03/day03_test.go
@@ -10,18 +10,23 @@ func Test_solve(t *testing.T) {
 		puzzle string
 	}
 	tests := []struct {
-		name string
-		args args
-		want int
+		name  string
+		args  args
+		want  int
+		want1 int
 	}{
-		{"example 1", args{"R8,U5,L5,D3\nU7,R6,D4,L4"}, 6},
-		{"example 2", args{"R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"}, 159},
-		{"example 3", args{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"}, 135},
+		{"example 1", args{"R8,U5,L5,D3\nU7,R6,D4,L4"}, 6, 30},
+		{"example 2", args{"R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"}, 159, 610},
+		{"example 3", args{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"}, 135, 410},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := solve(tt.args.puzzle); got != tt.want {
+			got, got1 := solve(tt.args.puzzle)
+			if got != tt.want {
 				t.Errorf("solve() = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("solve()1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
