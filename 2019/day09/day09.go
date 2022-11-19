8f66a64ecf70e84d8468a17ed6a3894aa8b742cb
Richard Clamp <richardc@unixbeard.net>
2022-11-19 12:55:00 +0000

    2019:9:2 boost it
    The puzzle description warns that this might be a longer runtime, but it
    seems we're not doing too badly enough on that front with our naiive
    implementation
    
      Day 9
          Part 1 3063082071
                 440.245µs
          Part 2 81348
                 95.044289ms

--- 2019/day09/day09.go
+++ 2019/day09/day09.go
@@ -14,23 +14,28 @@ var puzzle string
 func init() {
 	runner.Register(
 		runner.Solution{
-			Year: 2019,
-			Day:  9,
-			Input: func() any {
-				cpu := intcode.New(puzzle)
-				return &cpu
-			},
-			Part1: func(i any) any { return testMode(i.(*intcode.Cpu)) },
+			Year:  2019,
+			Day:   9,
+			Part1: func(i any) any { return testMode(puzzle) },
+			Part2: func(i any) any { return boost(puzzle) },
 		},
 	)
 }
 
-func testMode(c *intcode.Cpu) int {
+func testMode(program string) int {
+	c := intcode.New(program)
 	c.Input([]int{1})
 	c.Run()
 	out := c.Output()
-	if len(out) != 0 {
+	if len(out) > 1 {
 		fmt.Println(out)
 	}
 	return out[0]
+}
+
+func boost(program string) int {
+	c := intcode.New(program)
+	c.Input([]int{2})
+	c.Run()
+	return c.Output()[0]
 }
